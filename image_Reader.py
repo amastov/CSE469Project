37ebc40cb50a97a364ea77d784cdba77c0114040
joel82myhre <joel82myhre@gmail.com>
2017-04-29 13:56:15 -0700

    update to image reader

--- image_Reader.py
+++ image_Reader.py
@@ -4,20 +4,33 @@ import sys
 import os.path
 import hashlib
 import math
-import image
+
+import hashlib
+
+
 
 #read in Image file
 pathImage = []
-for args in sys.argv:
-    arg = args.split(' ')
-    pathImage.append(args)
+#    arg = args.split('/')
+#    pathImage.append(args)
 
-for pathImage[1] in pathImage:
-    try:
-        im=Image.open(pathImage[1])
-    # do stuff
-    except IOError:
-        print('Invalid Path')
-    # filename not an image file
+def extract_MD5_SH1(): 
+    pathImage = []
+    for args in sys.argv:
+        arg = args.split(' ')
+        pathImage.append(arg)
+    base=os.path.basename(pathImage[1][0])
+    name = os.path.splitext(base)
+    #base = base imageName, with extention
+    print(base)
+    name = name[0]
+    #Base is image name stripped of file type designation
+    print(name)
+    #
 
-print(pathImage)
+def main():
+    extract_MD5_SH1()
+    print('stuff')
+
+if __name__ == '__main__':
+	main()
