aa3845d47220cbd295583537f5e547a5400ff167
joel82myhre <joel82myhre@gmail.com>
2017-04-29 14:40:53 -0700

    sha1 and md5 text file out added

--- image_Reader.py
+++ image_Reader.py
@@ -26,11 +26,40 @@ def extract_MD5_SH1():
     name = name[0]
     #Base is image name stripped of file type designation
     print(name)
-    #
+    sha1Name = 'SHA1-' + name + '.txt'
+    md5Name = 'MD5-' + name + '.txt'
+    print(sha1Name)
+    print(md5Name)
+    #calculations go here
+    print (pathImage[1][0])
+    md5Checksum = md5(pathImage[1][0])
+    print(md5Checksum)
+    sha1Checksum = sha1(pathImage[1][0])
+    print(sha1Checksum)
+    text_fileSHA1 = open(sha1Name, "w")
+    text_fileSHA1.write(sha1Checksum)
+    text_fileSHA1.close()
+    text_fileMD5 = open(md5Name, "w")
+    text_fileMD5.write(md5Checksum)
+    text_fileMD5.close()
+    
+    
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+def sha1(fname):
+    hash_md5 = hashlib.sha1()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
 
 def main():
     extract_MD5_SH1()
-    print('stuff')
-
+    
 if __name__ == '__main__':
 	main()
